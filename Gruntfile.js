4d5b5423b8fa6b36b3efeea3c83c05d1a7c23508
Micah Lee <micah@micahflee.com>
2014-06-26 13:00:06 -0400

    added skeleton website

---
+++ Gruntfile.js
@@ -0,0 +1,138 @@
+/* jshint indent:2, node:true */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    copy: {
+      dist: {
+        files: [
+          { dest: 'dist/', src: ['.htaccess', 'favicon.ico'] },
+          { dest: 'dist/', src: 'assets/fonts/**' },
+          { dest: 'dist/', src: 'assets/img/**' }
+        ]
+      }
+    },
+
+    includereplace: {
+      dist: {
+        options: {
+          globals: {
+            version: '<%= pkg.version %>'
+          }
+        },
+        files: [
+          { src: '*.html', dest: 'dist/' }
+        ]
+      }
+    },
+
+    cssmin: {
+      compress: {
+        options: {
+          compatibility: 'ie8',
+          keepSpecialComments: 0,
+          report: 'min'
+        },
+        files: {
+          'dist/assets/css/pack-<%= pkg.version %>.css': [
+            'assets/css/normalize.css',
+            'assets/css/style.css'
+          ]
+        }
+      }
+    },
+
+    uglify: {
+      options: {
+        compress: {
+          warnings: false
+        },
+        mangle: true,
+        preserveComments: false,
+        report: 'min'
+      },
+      compress: {
+        files: {
+          'dist/assets/js/pack-<%= pkg.version %>.js': [
+          ]
+        }
+      }
+    },
+
+    htmlmin: {
+      dist: {
+        options: {
+          collapseWhitespace: true,
+          removeComments: true
+        },
+        expand: true,
+        cwd: 'dist',
+        dest: 'dist',
+        src: ['**/*.html']
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      files: ['assets/**/*', 'index.html', 'Gruntfile.js'],
+      tasks: 'build'
+    },
+
+    connect: {
+      server: {
+        options: {
+          base: 'dist/',
+          port: 4000
+        }
+      }
+    },
+
+    validation: {
+      options: {
+        charset: 'utf-8',
+        doctype: 'HTML5',
+        failHard: true,
+        reset: true
+      },
+      files: {
+        src: 'dist/**/*.html'
+      }
+    },
+
+    clean: {
+      dist: 'dist/'
+    }
+
+  });
+
+  // Load any grunt plugins found in package.json.
+  require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
+  require('time-grunt')(grunt);
+
+  grunt.registerTask('build', [
+    'clean',
+    'copy',
+    'includereplace',
+    'cssmin',
+    'uglify',
+    'htmlmin'
+  ]);
+
+  grunt.registerTask('test', [
+    'build',
+    'validation'
+  ]);
+
+  grunt.registerTask('default', [
+    'build',
+    'connect',
+    'watch'
+  ]);
+};
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-onionshare-website
-==================
+# OnionShare Website
+
+This is the code that powers <https://onionshare.org/>. It's licensed under [AGPLv3](/LICENSE).
+
+## Developing
+
+* Install [Node.js](http://nodejs.org/download/)
+* Install Grunt: `npm install -g grunt-cli`
+* Install the Node.js dependencies: `cd onionshare-website && npm install`
+* Run `grunt build` to build the static site, `grunt` to build and watch for changes (http://localhost:4000/)
